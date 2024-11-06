5f8b5ef368e535197cd2895f103ae2c9653cef9b
quliyev14 <elgun.gg2003@gmail.com>
2024-11-06 23:40:32 +0400

    Update

--- MarketDestkop/ViewModels/UserUserControlViewModel/UserViewModel.cs
+++ MarketDestkop/ViewModels/UserUserControlViewModel/UserViewModel.cs
@@ -1,30 +1,76 @@
-﻿using MarketWpfProject.Moduls;
+﻿using GalaSoft.MvvmLight.Command;
+using MarketWpfProject.Data;
+using MarketWpfProject.Helper.PathHelper;
+using MarketWpfProject.Moduls;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows;
 
 namespace MarketWpfProject.ViewModels.UserUserControlViewModel
 {
     public class UserViewModel : INotifyPropertyChanged
     {
+        private string path = "products.json";
+
         public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
+
+        private string? _searchTb;
+
+        public string? SearchTb
+        {
+            get => _searchTb;
+            set
+            {
+                _searchTb = value;
+                OnPropertyChanged(nameof(SearchTb));
+            }
+        }
+
+        public RelayCommand SearchCommand { get; set; }
 
         public UserViewModel()
         {
             LoadProduct();
+            SearchCommand = new RelayCommand(SearchProduct);
         }
 
         private void LoadProduct()
         {
-            Products.Add(new Product("Drink 4", 5.0m, 12, string.Empty));
-            Products.Add(new Product("Drink 2", 3.0m, 13, string.Empty));
-            Products.Add(new Product("Drink 3", 2.0m, 14, string.Empty));
-            Products.Add(new Product("Drink 4", 1.0m, 15, string.Empty));
-            Products.Add(new Product("Drink 5", 6.0m, 16, string.Empty));
-            Products.Add(new Product("Drink 6", 7.0m, 17, string.Empty));
-            Products.Add(new Product("Drink 7", 8.0m, 18, string.Empty));
+            if (PathCheck.OpenOrClosed(path))
+            {
+                var products = DB.JsonRead<Product>(path) ?? throw new ArgumentNullException("Argument is null!");
+                foreach (var product in products)
+                    Products.Add(product);
+            }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private void SearchProduct()
+        {
+            if (string.IsNullOrEmpty(SearchTb))
+            {
+                Products.Clear();
+                LoadProduct();
+                return;
+            }
+
+            Products.Clear();
+
+            if (PathCheck.OpenOrClosed(path))
+            {
+                var products = DB.JsonRead<Product>(path) ?? throw new ArgumentNullException("Argument is null!");
+
+                var matchedProducts = products.Where(p => p.Name!.ToLower().Contains(SearchTb.ToLower())).ToList();
+
+                if (matchedProducts.Any())
+                    foreach (var product in matchedProducts)
+                        Products.Add(product);
+                else
+                    MessageBox.Show("No products found with this search term.");
+            }
+            SearchTb = string.Empty;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
